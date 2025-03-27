8b274ec02bfa70c0b98e7e9c7278269f17b05f4d
ashesranjit <62844607+ashesranjit@users.noreply.github.com>
2025-03-27 14:19:06 +0200

    ashesranjit@gmail.com

---
+++ Lab1Q1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "Enter a string: ";
+    string str;
+    getline(cin, str);
+
+    for (size_t i = 0; i < str.length(); i++)
+    for (int i = 0; i < str.length(); i++)
+    {
+        str[i] = toupper(str[i]);
+    }
+
+    // Count the number of vowels
+    int count = 0;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+        {
+            count++;
+        }
+    }
+
+    // Reverse the string
+    string reversedStr = "";
+    for (int i = str.length() - 1; i >= 0; i--)
+    {
+        reversedStr += str[i];
+    }
+
+    // Print the results
+    cout << "Uppercase string: " << str << endl;
+    cout << "Number of vowels: " << count << endl;
+    cout << "Reversed string: " << reversedStr << endl;
+
+    return 0;
+}
